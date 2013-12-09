885baf66081ba7d17202c6b7ccdc4c2069306192
Nat Welch <nat@natwelch.com>
2013-12-09 10:42:30 -0800

    Adds comments about how to implement asciiarmor

--- lib/ascii_armor.rb
+++ lib/ascii_armor.rb
@@ -1,7 +1,24 @@
 class AsciiArmor
 
   # http://tools.ietf.org/search/rfc4880
-  def self.validate str
+  # Could easily be based off of https://github.com/bendiken/openpgp/blob/master/lib/openpgp/armor.rb#L75
+  def self.validate text
+    require "base64"
 
+    # Does the block have valid headers?
+    # TODO: implement
+    
+    # Split the message and checksum
+
+    # Is the text block length divisible by 4?
+    # TODO: implement
+
+    # Does the text block have valid characters
+    # TODO: implement
+    regex_64 = %r{[A-Za-z0-9+\/]+={0,3}}
+
+    # Validate Checksum
+    # https://en.wikipedia.org/wiki/Base64#OpenPGP
+    # TODO: implement
   end
 end
